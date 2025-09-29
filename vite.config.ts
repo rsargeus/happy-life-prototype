833f61845978823bb0bdc9ec14b1cea5abb71f20
robert <robert.sargeus@geshdo.com>
2025-09-30 00:30:58 +0200

    Updates and fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/happy-life-prototype/', // <-- viktigt för GitHub Pages under repo-namn
-})
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/happy-life-prototype/', // <-- viktigt för GitHub Pages
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
