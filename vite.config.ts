5469e843d5664548a85e8a431b0aee09fa3fe996
robert <robert.sargeus@geshdo.com>
2025-09-29 23:58:29 +0200

    Added github pages support

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/happy-life-prototype/', // <-- viktigt för GitHub Pages
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
